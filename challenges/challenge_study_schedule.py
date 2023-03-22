bb998ea696f5fc5790f0a5533b02cc0f9ea9face
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-03-22 16:09:50 -0300

    requisito 1 feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
+def validate_period(list):
+    for tupla in list:
+        if type(tupla[0]) != int or type(tupla[1]) != int:
+            return False
+        
+    return True
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) == int and validate_period(permanence_period):
+        count = 0
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                count += 1
+
+        return count
+
+    return None
