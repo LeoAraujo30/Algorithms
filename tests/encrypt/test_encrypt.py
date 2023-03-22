b3dd17f6051a26cefc03c457d746254483301c7a
LeoAraujo30 <leonardo.araujo300301@gmail.com>
2023-03-22 20:28:55 -0300

    requisito 2 feito

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    string = "python"
+
+    assert encrypt_message(string, 3) == "typ_noh"
+    assert encrypt_message(string, 4) == "no_htyp"
+    assert encrypt_message(string, 9) == "nohtyp"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(string, "")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)
